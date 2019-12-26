75356eba0964d31e7e2aa664e4941904c5fd6f08
Pranav Dixit <dprnav@gmail.com>
2019-12-26 17:54:41 +0530

    'css'

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
 
   constructor(private router: Router) { }
   searchCall(query){
-    this.router.navigate(['/search'], { queryParams: { query: query } });
+    this.router.navigate(['/search'], { queryParams: { query: query }, skipLocationChange: true });
   }
   ngOnInit() {
 
